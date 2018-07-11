a86a32d3f1361ef286ff75c65fde844a8c8d55b8
justoneliu <justone20@live.com>
2018-07-11 13:53:04 +0800

    add comment to the app.py file

--- www/app.py
+++ www/app.py
@@ -3,16 +3,28 @@ import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
-def index(request):
-    return web.Response(body=b'<h1>Awsome</h1>', content_type="text/html")
+#define a request handler, which will return a testing page
+def url_handler_testingpage(request):
+	return web.Response(body=b'<h1>Hi gay!</h1><p>you are visiting my testing web-page, just enjoy yourself.</p>', content_type="text/html")
+
+def url_handler_hello(request):
+	return web.Response(body=b'<h1>Hello frends</h1>', content_type="text/html")
+
 
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+	#create a Application instance
+	app = web.Application(loop=loop)
+	#register the request handle
+	app.router.add_route('GET', '/test', url_handler_testingpage)
+	app.router.add_route('GET', '/hello', url_handler_hello)
+	#建立服务器协程，监听9002端口
+	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9002)
+	#相当于print
+	logging.info('server started at http://127.0.0.1:9002')
+	return srv
 
+#创建异步框架的事件轮询实例
 loop = asyncio.get_event_loop()
+
 loop.run_until_complete(init(loop))
 loop.run_forever()

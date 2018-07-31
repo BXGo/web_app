35a4c4ed18a0a5df0c4d26d564545b178a6aaa8e
Justone Liu <justone20@live.com>
2018-08-01 02:31:56 +0800

    day 10 : add api to implement user regist, signin, authenticate(cookie, session, encrypt, sha1)

--- www/handlers.py
+++ www/handlers.py
@@ -18,11 +18,37 @@ _COOKIE_KEY = 'growing up is a gradual separation'
 
 
 def user2cookie(user, max_age):
-	"""计算加密cookie"""
+	"""加密cookie"""
 	expires = str(int(time.time() + max_age)) # 计算过期时间，以字符串返回
 	s = '%s-%s-%s-%s' %(user.id, user.passwd, expires, _COOKIE_KEY)
 	L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
 	return '-'.join(L)
+
+
+async def cookie2user(cookie_str):
+	'''解密cookie'''
+	if not cookie_str:
+		return None
+	try:
+		L = cookie_str.split('-')
+		if len(L) != 3:
+			return None
+		uid, expires, sha1 = L
+		if int(expires) < time.time():
+			# cookie过期
+			return None
+		user = await User.find(uid)
+		if user is None:
+			return None
+		s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+		if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+			logging.info('invalid sha1')
+			return None
+		user.passwd = '******'
+		return user
+	except Exception as e:
+		logging.exception(e)
+		return None
 
 
 @get('/')

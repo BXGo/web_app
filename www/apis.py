61c217dcf985f092dc56a755f1350a2ab8664db3
Justone Liu <justone20@live.com>
2018-08-01 02:05:22 +0800

    day 10 : add module to log api error

---
+++ www/apis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+' JSON API definition. '
+
+
+import json, logging, inspect, functools
+
+
+class APIError(Exception):
+	' the base APIError which contains error(required), data(optional) and message(optional). '
+	def __init__(self, error, data='', message=''):
+		super(APIError, self).__init__(message)
+		self.error = error
+		self.data = data
+		self.message = message
+
+
+class APIValueError(APIError):
+	'''indicate the input value has error or invalid.'''
+	def __init__(self, field, message=''):
+		super(APIValueError, self).__init__('value:invalid', field, message)
+
+
+class APIResourceNotFoundError(APIError):
+	"""indicate the resource was not found."""
+	def __init__(self, field, message=''):
+		super(APIResourceNotFoundError, self).__init__('value:notFound', field, message)
+		
+
+class APIPermissionError(APIError):
+	"""indicate the api has no permission."""
+	def __init__(self, message=''):
+		super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
+
+		
+		
